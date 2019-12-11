ca8a6c62dfc793327feb019622fb445843c22549
Michelle Van <mjtvan@gmail.com>
2019-12-10 20:50:58 -0500

    song selector done

---
+++ src/components/SongList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions';
+
+class SongList extends React.Component {
+    renderList = () => {
+        return this.props.songs.map(song => {
+            return (
+                <div className='item' key={song.title}>
+                    <div className='right floated content'>
+                        <button 
+                        className='ui button primary' 
+                        onClick ={() => this.props.selectSong(song)}
+                        >
+                            Select
+                        </button>
+                    </div>
+                    <div className='content'>
+                        {song.title}
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    render(){
+        //this.props === {songs: state.songs}
+        // console.log(this.props.songs)
+        // console.log(this.props)
+        return <div className='ui divided list'>{this.renderList()}</div>
+    }
+
+}
+
+const mapStateToProps = (state) => {
+    // console.log(state);
+    return { songs: state.songs };
+}
+//take state object and run some computation on 
+//it to cause data to make it props in our components
+//always gets state as argument and return state
+
+
+
+export default connect(mapStateToProps, {selectSong})(SongList);
+
+
